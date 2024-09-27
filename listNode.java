3245383527fde3c9b3140ab55d04a44499b33d1c
Dikshya singh <dikshyasingh99@gmail.com>
2024-09-27 21:46:53 +0530

    final commit

---
+++ listNode.java
@@ -0,0 +1,8 @@
+// Definition for singly-linked list.
+  public class listNode {
+      int val;
+      ListNode next;
+      listNode() {}
+      listNode(int val) { this.val = val; }
+      listNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
