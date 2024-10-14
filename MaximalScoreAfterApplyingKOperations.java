b291a3b863d506c4f6e0c3909e43daf6f1a42f9b
Dikshya singh <dikshyasingh99@gmail.com>
2024-10-14 11:36:21 +0530

    final commit

---
+++ MaximalScoreAfterApplyingKOperations.java
@@ -0,0 +1,34 @@
+import java.util.PriorityQueue;
+
+public class MaximalScoreAfterApplyingKOperations {
+    public static long maxKelements(int[] nums, int k) {
+        PriorityQueue<Long> maxHeap = new PriorityQueue<>((a, b) -> Long.compare(b, a));
+
+        // Insert all elements into the max-heap
+        for (int num : nums) {
+            maxHeap.add((long) num);
+        }
+
+        long score = 0;
+
+        // Perform k operations
+        for (int i = 0; i < k; i++) {
+            long maxVal = maxHeap.poll();
+
+            // Add the largest value to the score
+            score += maxVal;
+
+            // Replace the number with ceil(maxVal / 3)
+            maxHeap.add((maxVal + 2) / 3);  // Using (maxVal + 2) / 3 to simulate ceil
+        }
+
+        return score;
+    }
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 7};
+        int k = 4;
+
+        long result = maxKelements(nums, k);
+        System.out.println("Maximal score after " + k + " operations: " + result);
+    }
+}
